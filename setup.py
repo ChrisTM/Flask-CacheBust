aa09ad861f104f987928fe9f5107ccfe0e4473c3
Chris Mitchell <chris@cloudboltsoftware.com>
2015-06-23 23:21:48 -0400

    add some extra info to barebones setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 setup(
     name='Flask-CacheBust',
     version='1.0.0',
+    description='Flask extension that cache-busts static files',
     packages=['flask_cache_bust'],
+    license='MIT',
+    url='https://github.com/ChrisTM/Flask-CacheBust',
+    install_requires=[
+        'Flask',
+    ],
 )
